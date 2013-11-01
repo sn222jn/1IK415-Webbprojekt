a4dfc17167fedb0e89cad41b3067785ba4bb2b2d
Sylvester Norrbjork <sn222jn@student.lnu.se>
2013-11-01 19:12:52 +0100

    Added CSS transistion effects among other things

---
+++ js/slider.js
@@ -0,0 +1,39 @@
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//  Inspired by http://codepen.io/dropside/pen/bxhke  //
+//  Images from http://certaine.tumblr.com/           //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+$(document).ready(function(){
+  var isWebkit = navigator && navigator.userAgent.match(/webkit/i);
+  var $root = $(isWebkit ? 'body' : 'html');
+  var elements = $('div'), elcount = elements.length;
+  var scrolling = false;
+  // Replacing the CSS attr(... url)
+  elements.css('background-image', function(i){
+    return 'url('+$(this).data('img')+')';
+  });
+  //Add permalinks
+  elements.each(function(i){
+    var $t = $(this);
+    var id = $t.attr('id');
+    if(!id) return;
+    $('<a>').addClass('permalink')
+            .attr('href', '#'+id)
+            .appendTo($t);
+  });
+  $root.keydown(function(e){
+    if(e.keyCode != 37 && e.keyCode != 39) return;
+    var current = scrolling || 0;
+    if(scrolling === false)
+    {
+      var bsT = $root.scrollTop(), t;
+      while(current < elcount && (t = elements.eq(current).offset().top) < bsT)
+        current++;
+    }
+    if(e.keyCode == 37) current--;
+    else if(scrolling !== false || t == bsT) current++;
+    current = (current + elcount) % elcount;
+    $root.stop().animate({scrollTop: elements.eq(current).offset().top}, function(){scrolling = false;});
+    scrolling = current;
+  });
+});
